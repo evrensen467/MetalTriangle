8da5605fdd600d37c59d8bcd0021fe15922e222f
Evren Sen <contact@evrsen.dev>
2024-07-03 02:04:36 +0200

    Initial Commit

---
+++ MetalTriangle/MetalTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTriangleApp.swift
+//  MetalTriangle
+//
+//  Created by Evren Sen on 2024-07-03.
+//
+
+import SwiftUI
+
+@main
+struct MetalTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
